7679ed87957969fe26a621887d7f042f7f342fc7
Aaron <amark@g.jct.ac.il>
2026-08-31 21:13:39 +0300

    oryx/jsonx: update stale json-patch upstream reference
    Fixes #1844
    
    Signed-off-by: Aaron <amark@g.jct.ac.il>

--- oryx/jsonx/patch.go
+++ oryx/jsonx/patch.go
@@ -60,7 +60,7 @@ func ApplyJSONPatch[T any](p json.RawMessage, object T, denyPaths ...string) (re
 	}
 
 	for _, op := range patch {
-		// Some operations are buggy, see https://github.com/evanphx/json-patch/pull/158
+		// Reject unsupported operations (test/copy/move) that json-patch handles poorly.
 		if isUnsupported(op) {
 			return result, errors.Errorf("unsupported operation: %s", op.Kind())
 		}
